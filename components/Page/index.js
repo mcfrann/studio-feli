88a0c392cc546746302968db8013a8e265e430b6
Francesca <fmcconnell59@gmail.com>
2024-03-29 15:29:16 -0600

    add base page component and style file

---
+++ components/Page/index.js
@@ -0,0 +1,8 @@
+import styles from "./style.module.scss";
+import Head from "next/head";
+
+const Page = () => {
+  return <section className={styles.page}>PAGE</section>;
+};
+
+export default Page;
