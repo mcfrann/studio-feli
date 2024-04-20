26456bd9f0b4b62c86ea44c58e1d526320176804
Francesca <fmcconnell59@gmail.com>
2024-04-19 20:16:55 -0600

    create base hero component

---
+++ components/Page/Hero/index.js
@@ -0,0 +1,12 @@
+import styles from "./style.module.scss";
+import Logo from "../../SVGs/Logo/logo";
+
+const Hero = () => {
+  return (
+    <section className={styles.hero}>
+      <Logo />
+    </section>
+  );
+};
+
+export default Hero;
