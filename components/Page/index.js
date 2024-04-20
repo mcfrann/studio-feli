2971a2c4acff22ec0bb4faa8be19f4eaff289447
Francesca <fmcconnell59@gmail.com>
2024-04-19 20:17:04 -0600

    import and render hero in page

--- components/Page/index.js
+++ components/Page/index.js
@@ -1,11 +1,13 @@
 import styles from "./style.module.scss";
 import Header from "./Header";
 import Footer from "./Footer";
+import Hero from "./Hero";
 
 const Page = () => {
   return (
     <section className={styles.page}>
       <Header />
+      <Hero />
       <Footer />
     </section>
   );
