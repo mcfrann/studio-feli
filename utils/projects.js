20693f5526f638669a1769340a5c5679aa79afad
Francesca <fmcconnell59@gmail.com>
2024-04-22 20:28:33 -0600

    import project images

--- utils/projects.js
+++ utils/projects.js
@@ -1,5 +1,9 @@
+import sia from "../public/images/sia.jpg";
+import luna from "../public/images/Luna.jpg";
+import dromo from "../public/images/restaurant.jpg";
+
 export const projects = [
-  { name: "Spontaneous Sia", image: "", description: "" },
-  { name: "Luna", image: "", description: "" },
-  { name: "Dromo", image: "", description: "" },
+  { name: "Spontaneous Sia", image: sia, description: "" },
+  { name: "Luna", image: luna, description: "" },
+  { name: "Dromo", image: dromo, description: "" },
 ];
