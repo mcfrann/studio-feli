4c012808b78ceca97adf85c4b21f454e9652f108
Francesca <fmcconnell59@gmail.com>
2024-04-24 15:33:42 -0600

    add base bio section

---
+++ components/BioSection/index.js
@@ -0,0 +1,18 @@
+import styles from "./style.module.scss";
+import { bios } from "../../utils/bios";
+import BioBlock from "./BioBlock";
+
+const BioSection = () => {
+  const renderBios = () => {
+    return bios.map((bio, i) => {
+      let alternate = i % 2 === 0 || i === 0;
+      return (
+        <BioBlock bio={bio} alternate={alternate} key={`bio-block-${i}`} />
+      );
+    });
+  };
+
+  return <section className={styles.bioSection}>{renderBios()}</section>;
+};
+
+export default BioSection;
