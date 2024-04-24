d9cf95da2c1f6966e3782754c131d06ec0b2361f
Francesca <fmcconnell59@gmail.com>
2024-04-24 15:33:13 -0600

    add bios utils

---
+++ utils/bios.js
@@ -0,0 +1,24 @@
+import sia from "../public/bioImages/sia.jpg";
+import ivana from "../public/bioImages/ivana.jpg";
+import francesca from "../public/bioImages/francesca.jpg";
+
+export const bios = [
+  {
+    name: "Francesca McConnell",
+    title: "Creative Developer",
+    copy: "As a developer and photographer, she blends code and visual storytelling, bringing a dual perspective to create seamless and creative projects.",
+    image: francesca,
+  },
+  {
+    name: "Ivana Musutova",
+    title: "Visual Designer",
+    copy: "As a developer and photographer, she blends code and visual storytelling, bringing a dual perspective to create seamless and creative projects.",
+    image: ivana,
+  },
+  {
+    name: "Sia",
+    title: "CEO",
+    copy: "As a developer and photographer, she blends code and visual storytelling, bringing a dual perspective to create seamless and creative projects.",
+    image: sia,
+  },
+];
