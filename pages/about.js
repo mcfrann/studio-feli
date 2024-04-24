efe1e0fe46094eeff31c149f92a2b42778316b71
Francesca <fmcconnell59@gmail.com>
2024-04-24 16:45:34 -0600

    import / render about section

--- pages/about.js
+++ pages/about.js
@@ -1,16 +1,20 @@
 import Page from "../components/Page";
 import Hero from "../components/Hero";
 import BioSection from "../components/BioSection";
+import AboutSection from "../components/AboutSection";
+import { about } from "../utils/aboutText";
 
 export default function About() {
   const text = {
     headline: "Who We Are",
     copy: "As a dynamic woman duo with a shared passion for design and collaboration, we thrive in the intersection of creativity and technology.",
   };
+
   return (
     <Page>
       <Hero text={text} />
       <BioSection />
+      <AboutSection text={about} />
     </Page>
   );
 }
