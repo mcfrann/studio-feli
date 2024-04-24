f05209788ae5a196640447f10495a7e395c7d649
Francesca <fmcconnell59@gmail.com>
2024-04-24 15:01:58 -0600

    move component imports/renders to home

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
 import styles from "../styles/Home.module.scss";
 import Page from "../components/Page";
+import Hero from "../components/Hero";
+import ServiceBar from "../components/ServiceBar";
+import About from "../components/About";
+import ProjectsSection from "../components/ProjectsSection";
+import FeaturedNav from "../components/FeaturedNav";
 
 export default function Home() {
   return (
     <section className={styles.homepageContainer}>
-      <Page />
+      <Page>
+        <Hero />
+        <ServiceBar />
+        <ProjectsSection />
+        <About />
+        <FeaturedNav />
+      </Page>
     </section>
   );
 }
