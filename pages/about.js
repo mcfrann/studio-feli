a1dd3994e12da2e69fda3534b22414b565c6278b
Francesca <fmcconnell59@gmail.com>
2024-05-07 17:35:19 -0600

    add scrollTrigger.refresh() to pages

--- pages/about.js
+++ pages/about.js
@@ -3,8 +3,17 @@ import Hero from "../components/Hero";
 import BioSection from "../components/BioSection";
 import AboutSection from "../components/AboutSection";
 import { about } from "../utils/aboutText";
+import { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 export default function About() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  useEffect(() => {
+    ScrollTrigger.refresh();
+  }, []);
+
   const text = {
     headline: "Who We Are",
     copy: "As a dynamic woman duo with a shared passion for design and collaboration, we thrive in the intersection of creativity and technology.",
--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,17 @@ import ProjectsSection from "../components/ProjectsSection";
 import FeaturedNav from "../components/FeaturedNav";
 import { servicesBar } from "../utils/barText";
 import { home } from "../utils/aboutText";
+import { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 export default function Home() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  useEffect(() => {
+    ScrollTrigger.refresh();
+  }, []);
+
   return (
     <section className={styles.homepageContainer}>
       <Page>
